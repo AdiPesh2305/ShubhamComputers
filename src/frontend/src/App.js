e647372295806ac0e04f8046372d036a001386ed
Kalpeshkumar Jain <kalpeshjain89@gmail.com>
2022-09-03 17:58:10 +0530

    Added references

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import "./styles/App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { GlobalCartContextProvider } from "./context/CartContext";
+import loadingIcon from "./assets/images/dashboardloader3.gif";
+const Homepage = lazy(() => import("./Components/Homepage"));
+const Shopping = lazy(() => import("./Components/Shopping"));
+const Cart = lazy(() => import("./Components/Cart/Cart"));
+const Catalog = lazy(() => import("./Components/Catalog"));
+
+const SearchProducts = lazy(() => import("./Components/SearchProducts"));
+const PageNotFound = lazy(() => import("./Components/PageNotFound"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalCartContextProvider>
+        <Router>
+          <Suspense
+            fallback={
+              <img src={loadingIcon} alt="loading" className="loadingIcon" />
+            }
+          >
+            <Routes>
+              <Route exact path="/" element={<Homepage/>} />
+              <Route exact path="/shop" element={<Shopping/>} />
+              <Route exact path="/search" element={<SearchProducts/>} />
+              <Route exact path="/cart" element={<Cart/>} />
+              <Route exact path="/trend/:collectionname" element={<Shopping/>} />
+              <Route exact path="/collections" element={<Shopping/>} />
+              <Route
+                exact
+                path="/collections/:collectionname"
+                element={<Shopping/>}
+              />
+              <Route
+                exact
+                path="/catalog/item/:id/:productname/:action"
+                element={<Catalog/>}
+              />
+
+              <Route element={<PageNotFound/>} />
+            </Routes>
+          </Suspense>
+        </Router>
+    </GlobalCartContextProvider>
   );
 }
 
