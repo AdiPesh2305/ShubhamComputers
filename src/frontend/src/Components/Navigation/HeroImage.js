e65f3091be09abbd0a31aa670cef5c87a0898246
Kalpeshkumar Jain <kalpeshjain89@gmail.com>
2023-04-23 22:46:34 +0530

    Lazy load HeroImage

--- src/frontend/src/Components/Navigation/HeroImage.js
+++ src/frontend/src/Components/Navigation/HeroImage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../../styles/HeroImage.scss";
 import HeroImg from '../../assets/images/hero-image.jpg';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export default function HeroImage() {
-
   return (
     <div className='hero-wrapper'>
-      <img
+      <LazyLoadImage
         src={HeroImg}
         alt=""
+        effect="blur"
       />
       <h1>SHUBHAM COMPUTERS</h1>
       <p>COMPUTER ACCESSORIES and more...</p>
